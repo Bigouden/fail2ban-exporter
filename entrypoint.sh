153683912c42544cd5a3ed99435e17d64e960fbd
Thomas GUIRRIEC <thomas@guirriec.fr>
2023-12-10 17:46:53 +0000

    Initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 # shellcheck source=/dev/null
+
+if [ -z "${FAIL2BAN_SOCAT_HOST}" ] && [ -z "${FAIL2BAN_SOCAT_FILE}" ]; then
+	true
+else
+	echo "Socat : ${FAIL2BAN_SOCAT_FILE} -> ${FAIL2BAN_SOCAT_HOST}"
+	socat UNIX-LISTEN:"${FAIL2BAN_SOCAT_FILE}",fork TCP:"${FAIL2BAN_SOCAT_HOST}" &
+fi
+
 . "${VIRTUAL_ENV}"/bin/activate
 python3 "${SCRIPT}"
